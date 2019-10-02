315aa8a96de2d15fbf82b9ca7d09789cb1d4e26e
gimadutdin <33692565+gimadutdin@users.noreply.github.com>
2019-10-02 23:41:44 +0300

    Add files via upload

--- matstat/z2.r
+++ matstat/z2.r
@@ -5,4 +5,5 @@ data_file = read.csv("r1z1.csv")
 x = data_file$X
 n = length(x)
 
-hist(x, main = "Частотная гистограмма", xlab = "Значение", ylab = "Частота", xlim = c(105, 130), breaks = 20, col = "lightblue")
+hist(x, main = "Гистограмма выборки", xlab = "Значение", ylab = "Вер-ть", xlim = c(105, 130), breaks = 20, col = "lightblue", freq = FALSE)
+lines(density(x), col = "red", lwd = 3)
